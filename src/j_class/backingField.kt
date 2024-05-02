49b7e07d381a48753442ad4af1665d290405ac45
KimYong91 <rladyd97@naver.com>
2024-05-02 11:01:22 +0900

    backingField

--- src/j_class/backingField.kt
+++ src/j_class/backingField.kt
@@ -5,7 +5,7 @@ class People(name: String, var age: Int) {
     // 만약 입력받은 이름을 대문자로 사용하고 싶으면
     // 커스텀 게터 사용
     val name: String = name
-        // get() = name.uppercase()     // 커스텀 게터 안에 무한루프에 빠지기 때문
+        // get() = name.uppercase()     // 커스텀 게터 안에 '프로퍼티명'을 그대로 넣으면 무한루프에 빠지게 됨
         get() = field.uppercase()        // backingField 사용
         // 프로퍼티(게터 포함)이 아닌 필드 자기 자신을 가리키는 키워드
 }
