3c3098822009cadd9d5d7d3d378e4c57e146e686
KimYong91 <rladyd97@naver.com>
2024-05-03 16:30:40 +0900

    람다식 자바 비교

---
+++ src/q_lambda/FruitMain.java
@@ -0,0 +1,30 @@
+package q_lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FruitMain {
+    public static void main(String[] args) {
+        List<Fruit> fruits = Arrays.asList(
+                new Fruit("사과", 3000),
+                new Fruit("사과", 4000),
+                new Fruit("사과", 5000),
+                new Fruit("포도", 6000),
+                new Fruit("바나나", 3000)
+        );
+
+        // 람다식에서 사용되는 변수는 final (불변)이거나
+        // final에 가까워야 한다. (변경되는 지점이 없어야 한다.)
+        String targetFruitName = "사과";
+        targetFruitName = "바나나";
+
+//        List<Fruit> filtered = filterFruits(fruits,
+//                fruit -> targetFruitName.equals(fruit.getName()));
+    }
+
+    private static List<Fruit> filterFruits(List<Fruit> fruits, Predicate<Fruit> fruitFilter) {
+        return fruits.stream().filter(fruitFilter).toList();
+    }
+
+}
