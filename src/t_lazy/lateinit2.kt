a8a965027500cd0b8a8bf235769890e630584902
KimYong91 <rladyd97@naver.com>
2024-05-07 15:31:02 +0900

    ateinit 사용 제약 사항

---
+++ src/t_lazy/lateinit2.kt
@@ -0,0 +1,9 @@
+package t_lazy
+
+class LateConstraint {
+    /*  lateinit 사용 제약 사항
+    lateinit val name: String   // val 키워드 사용 불가
+    lateinit var age: Int       // 기본 타입(Int, Double, Boolean) 사용 불가
+    lateinit var nickName: String? // nullable 타입 사용 불가
+    */
+}
